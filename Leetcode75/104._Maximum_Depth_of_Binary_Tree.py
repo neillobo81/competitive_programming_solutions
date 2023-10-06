58166ef36aeba68ede3189c03df84d776bb722e6
Neil <neillobo81@gmail.com>
2023-10-05 22:09:54 -0400

    add more docs

--- Leetcode75/104._Maximum_Depth_of_Binary_Tree.py
+++ Leetcode75/104._Maximum_Depth_of_Binary_Tree.py
@@ -1,3 +1,8 @@
+# 104. Maximum Depth of Binary Tree
+#Easy
+#problem statement: https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
